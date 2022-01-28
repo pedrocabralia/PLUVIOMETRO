a48efe28131ea188256d010bf574596e5502d0a4
pedrocabralia <pedrocabralia@hotmail.com>
2022-01-27 21:44:19 -0300

    criando layout da página inicial

---
+++ pluviometro-app/resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>PLUVIOMETER</title>
+</head>
+<body>
+    
+    <header class="header">
+
+            <div class="top">
+               <img src="/img/logo.png" alt="logo" srcset="" class="logo">  
+               <p>PLUVIOMETER</p>
+            </div>
+
+    </header>
+
+    <main>
+        <p>content</p>
+    </main>
+  
+     <div class="menu">
+         <p>menu</p>
+     </div>
+    <footer>
+          <p>footer</p>
+    </footer>
+</body>
+</html>
